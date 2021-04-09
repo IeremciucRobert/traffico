7bdf3d2eb699e62861721309a84dd7adeac659f1
RobertIeremciuc <rob@codkom.com>
2021-04-09 12:16:27 +0300

    finished footer

--- src/App/Footer.js
+++ src/App/Footer.js
@@ -1,16 +1,63 @@
 import React from "react";
 import "../CSS/footer.css";
+import logo from "../Images/logo.svg";
+import facebook from "../Images/facebook.svg";
+import instagram from "../Images/instagram.svg";
+import twitter from "../Images/twitter.svg";
+import treckmobil from "../Images/treckmobil.svg";
 
 export default function Footer() {
   return (
     <footer>
-      <div className="width_container">
-        <div>
-          <div>
+      <div className="footer_treckmobil">
+        <img src={treckmobil} />
+      </div>
+      <div className="footer_background"></div>
+      <div className="footer_container width_container">
+        <div className="footer_form_container">
+          <div className="footer_text_outlined">
             We provide traffic management consultants so you get started
             quickly, contact us for a quote today!
           </div>
-          <form></form>
+          <form className="footer_form">
+            <span>Name</span>
+            <input placeholder="Your name" />
+            <span>Email address</span>
+            <input placeholder="Your email address" />
+            <button className="button_arrow_right">Get started</button>
+          </form>
+        </div>
+        <div className="footer_nav_container">
+          <nav className="top_nav">
+            <img className="top_logo" src={logo} />
+            <ul className="footer_buttons_container top_buttons_container">
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">How to</a>
+              </li>
+              <li>
+                <a href="#">Faqs</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <div className="footer_socials_container">
+        <div className="footer_socials_wrapper width_container">
+          <span>Copyright @ 2019</span>
+          <span className="footer_socials">
+            <a href="#">
+              <img src={facebook} />
+            </a>
+            <a href="#">
+              <img src={twitter} />
+            </a>
+            <a href="#">
+              <img src={instagram} />
+            </a>
+          </span>
         </div>
       </div>
     </footer>
