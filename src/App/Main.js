b337dd840c6f623dc7e8fb66be5469d5e7cc38bb
RobertIeremciuc <rob@codkom.com>
2021-04-15 09:34:56 +0300

    minor fix for netlify

--- src/App/Main.js
+++ src/App/Main.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "../CSS/main.css";
 import aboutImage from "../Images/about.svg";
 import applyImage from "../Images/apply.svg";
